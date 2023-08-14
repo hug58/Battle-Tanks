1d26a98b4aaf20f05addc675845cf78900a8ef72
Hugo <hugomontaez@gmail.com>
2023-08-14 04:57:29 -0400

    fixs tabs

--- scripts/camera.py
+++ scripts/camera.py
@@ -2,30 +2,27 @@ import pygame as pg
 
 
 class Camera:
-	def __init__(self,width,height,screen_size):
-		self.camera = pg.Rect((0,0),(width,height))
-		self.width = width
-		self.height = height
-		self.screen_size = screen_size
-	
-	def apply(self,entity):
-		return entity.rect.move(self.camera.topleft)
-	def apply_rect(self,rect):
-		#mueve la posición de la surface a la pos de la camara en topleft (arriba/izquierda)
-		return rect.move(self.camera.topleft)
-	
-	def update(self,target):
-		#Targe en negativo para que en caso de llegar al extremo left (positivo) , el movimiento sea 0
-		x = -target.rect.x + int(self.screen_size[0]/2)
-		y = -target.rect.y + int(self.screen_size[1]/2)
-		#print(x)
-		#limit scrolling to map size
-		x = min(0,x) #left
-		y = min(0,y) #top
-		#print(self.width)
-		#lógica inversa
-		#Si -(self.width - WIDTH) es menor que X, X seguira avanzando, en caso contrario X se mantendrá fijo
-		x = max(-(self.width - self.screen_size[0]),x)#right
-		y = max(-(self.height-self.screen_size[1]),y)
-		self.camera = pg.Rect(x,y,self.width,self.height)
+    ''' The camera object that will be used to render the scene and interact '''
+    def __init__(self,width,height,screen_size):
+        self.camera = pg.Rect((0,0),(width,height))
+        self.width = width
+        self.height = height
+        self.screen_size = screen_size
+    def apply(self,entity):
+        ''' Apply the camera transformation to the entity '''
+        return entity.rect.move(self.camera.topleft)
+    def apply_rect(self,rect):
+        ''' mueve la posición de la surface a la pos de la camara en topleft (arriba/izquierda)'''
+        return rect.move(self.camera.topleft)
+    def update(self,target):
+        '''Targe en negativo para que en caso de llegar al extremo left (positivo) , 
+        el movimiento sea 0'''
+        x_pos = -target.rect.x + int(self.screen_size[0]/2)
+        y_pos = -target.rect.y + int(self.screen_size[1]/2)
+        #limit scrolling to map size
+        x_pos = min(0,x_pos) #left
+        y_pos = min(0,y_pos) #top
+        x_pos = max(-(self.width - self.screen_size[0]),x_pos)
+        y_pos = max(-(self.height-self.screen_size[1]),y_pos)
+        self.camera = pg.Rect(x_pos,y_pos,self.width,self.height)
 
--- scripts/elements.py
+++ scripts/elements.py
@@ -3,24 +3,21 @@ import pygame as pg
 from scripts import ROUTE
 
 class Brick(pg.sprite.Sprite):
-
-	def __init__(self,x,y,width,height):
-
-		pg.sprite.Sprite.__init__(self)
-
-		self.box_img = pg.image.load(ROUTE("ASSETS/images/tiles.png"))
-		self.image = self.box_img.subsurface((0,0),(32,32))
-		self.rect = pg.Rect((x,y),(width,height))
-
+    ''' Class representing a Brick object '''
+    def __init__(self,x_pos,y_pos,width,height):
+        pg.sprite.Sprite.__init__(self)
+        self.box_img = pg.image.load(ROUTE("ASSETS/images/tiles.png"))
+        self.image = self.box_img.subsurface((0,0),(32,32))
+        self.rect = pg.Rect((x_pos,y_pos),(width,height))
 
 
 class Block(pg.sprite.Sprite):
-
-	def __init__(self,x,y,width,height):
-		pg.sprite.Sprite.__init__(self)
-		self.rect = pg.Rect((x,y),(width,height))
+    ''' Class representing a Block object '''
+    def __init__(self,x_pos,y_pos,width,height):
+        pg.sprite.Sprite.__init__(self)
+        self.rect = pg.Rect((x_pos,y_pos),(width,height))
 
 
 
 if __name__ == '__main__':
-    pass 
+	pass 
