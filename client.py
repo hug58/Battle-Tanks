e6dfc05f0670392968b6977c377bc62358ab8a8a
Hugo <hugomontaez@email.com>
2024-11-10 20:47:33 -0400

    delete pycache

--- client.py
+++ client.py
@@ -3,29 +3,15 @@
 import socket
 import sys
 import pygame as pg
-import asyncio
-
 from scripts import Text, ROUTE
 from scripts.menu import Menu
-
-
-def scan_ports(ip, start_port, end_port):
-    """ SCANNING PORTS FOR GAME STATUS """
-    print(f"Escaneando puertos en {ip} desde {start_port} hasta {end_port}...")
-    for port in range(start_port, end_port + 1):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(1)  # Establece un tiempo de espera de 1 segundo
-        result = sock.connect_ex((ip, port))  # Intenta conectar al puerto
-        if result == 0:
-            print(f"Puerto {port} está abierto")
-        sock.close()
 
 
 def load_bullet(bullet):
     """ load image from file """
     return (pg.image.load(ROUTE(bullet[0])),bullet[1])
 
-async def main():
+def main():
     """ Client game of server"""
 
     map_tmp = 'ASSETS/maps/zone_0.tmx'
@@ -33,7 +19,6 @@ async def main():
     SCREEN = pg.display.set_mode((WIDTH,HEIGHT + 36))
     clock = pg.time.Clock()
     main_surface = pg.Surface((WIDTH,HEIGHT))
-
     menu = Menu(SCREEN,map_tmp)
     game = menu.update(main_surface)
 
@@ -71,5 +56,4 @@ async def main():
         pg.display.flip()
 
 if __name__ == '__main__':
-    asyncio.run(main())
-    pg.quit()
+    main()
